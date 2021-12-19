d58bd8b36462066d5060795acdbec70300400a18
prakashz <mydreamzjobz@gmail.com>
2021-12-19 21:22:26 +0530

    1st commit - project from header to hot deals container

---
+++ javascript/index.js
@@ -0,0 +1,207 @@
+let i = 1;
+
+document.getElementById(`img_slider_image_container_${3}`).style.display = "block";
+document.getElementById(`chage_slide_btn_${3}`).style.backgroundColor="#45b2ff";
+document.getElementById(`chage_slide_btn_${3}`).style.color="white";
+
+setInterval(function() {
+   
+    if(i===1) {
+        document.getElementById(`img_slider_image_container_${3}`).style.display = "none";
+        document.getElementById(`img_slider_image_container_${1}`).style.display = "block";
+        document.getElementById(`chage_slide_btn_${3}`).style.color="black";
+        document.getElementById(`chage_slide_btn_${1}`).style.color="white";
+        document.getElementById(`chage_slide_btn_${3}`).style.backgroundColor="transparent";
+        document.getElementById(`chage_slide_btn_${1}`).style.backgroundColor="#45b2ff";
+        document.getElementById("image_slider_words_container_one_text1").style.transform = "translate(-400px, 0px)";
+        document.getElementById("image_slider_words_container_one_text2").style.transform = "translate(-400px, 0px) rotate(90deg)";
+        document.getElementById("image_slider_words_container_two_text1").style.transform = "translate(-450px, 0px)";
+        document.getElementById("image_slider_words_container_two_text2").style.transform = "scaleX(0)";
+        document.getElementById("image_slider_words_container_three_text1").style.transform = "translate(-400px, 0px)";
+        document.getElementById("image_slider_words_container_three_text2").style.transform = "translate(-400px, 0px) rotate(90deg)";
+        document.getElementById("image_slider_words_container_three_text3").style.transform = "translate(-350px, 0px)";
+    }
+    if(i===2) {
+        document.getElementById(`img_slider_image_container_${1}`).style.display = "none";
+        document.getElementById(`img_slider_image_container_${2}`).style.display = "block";
+        document.getElementById(`chage_slide_btn_${1}`).style.color="black";
+        document.getElementById(`chage_slide_btn_${2}`).style.color="white";
+        document.getElementById(`chage_slide_btn_${1}`).style.backgroundColor="transparent";
+        document.getElementById(`chage_slide_btn_${2}`).style.backgroundColor="#45b2ff";
+        document.getElementById("image_slider_words_container_one_text1").style.transform = "translate(-400px, 0px)";
+        document.getElementById("image_slider_words_container_one_text2").style.transform = "translate(-400px, 0px) rotate(90deg)";
+        document.getElementById("image_slider_words_container_two_text1").style.transform = "translate(-450px, 0px)";
+        document.getElementById("image_slider_words_container_two_text2").style.transform = "scaleX(0)";
+        document.getElementById("image_slider_words_container_three_text1").style.transform = "translate(-400px, 0px)";
+        document.getElementById("image_slider_words_container_three_text2").style.transform = "translate(-400px, 0px) rotate(90deg)";
+        document.getElementById("image_slider_words_container_three_text3").style.transform = "translate(-350px, 0px)";
+    }
+    if(i===3) {
+        document.getElementById(`img_slider_image_container_${2}`).style.display = "none";
+        document.getElementById(`img_slider_image_container_${3}`).style.display = "block";
+        document.getElementById(`chage_slide_btn_${2}`).style.color="black";
+        document.getElementById(`chage_slide_btn_${3}`).style.color="white";
+        document.getElementById(`chage_slide_btn_${2}`).style.backgroundColor="transparent";
+        document.getElementById(`chage_slide_btn_${3}`).style.backgroundColor="#45b2ff";
+        document.getElementById("image_slider_words_container_one_text1").style.transform = "translate(-400px, 0px)";
+        document.getElementById("image_slider_words_container_one_text2").style.transform = "translate(-400px, 0px) rotate(90deg)";
+        document.getElementById("image_slider_words_container_two_text1").style.transform = "translate(-450px, 0px)";
+        document.getElementById("image_slider_words_container_two_text2").style.transform = "scaleX(0)";
+        document.getElementById("image_slider_words_container_three_text1").style.transform = "translate(-400px, 0px)";
+        document.getElementById("image_slider_words_container_three_text2").style.transform = "translate(-400px, 0px) rotate(90deg)";
+        document.getElementById("image_slider_words_container_three_text3").style.transform = "translate(-350px, 0px)";
+    }
+    i++;
+    if(i > 3) {
+        i = 1;
+    }
+}, 3000);
+
+setInterval(function() {
+    document.getElementById("image_slider_words_container_one_text1").style.transform="translate(0px, 0px)";
+    document.getElementById("image_slider_words_container_one_text2").style.transform = "translate(0px, 0px) rotate(0deg)";
+    document.getElementById("image_slider_words_container_two_text1").style.transform = "translate(0px, 0px)";
+    document.getElementById("image_slider_words_container_two_text2").style.transform = "scaleX(1)";
+    document.getElementById("image_slider_words_container_three_text1").style.transform = "translate(0px, 0px)";
+    document.getElementById("image_slider_words_container_three_text2").style.transform = "translate(0px, 0px) rotate(0deg)";
+    document.getElementById("image_slider_words_container_three_text3").style.transform = "translate(0px, 0px)";
+}, 3000);
+
+document.getElementById("table_navbar_showing_container").addEventListener("click", function() {
+    if(document.getElementsByClassName("hamburger_main_container_table")[0].style.display=="block") {
+        document.getElementsByClassName("hamburger_main_container_table")[0].style.display="none";
+        document.getElementById("hamburger's_arrow_icon").classList = "fas fa-chevron-circle-down";
+        
+    } else {
+        document.getElementsByClassName("hamburger_main_container_table")[0].style.display="block";
+        document.getElementById("hamburger's_arrow_icon").classList = "fas fa-chevron-circle-up";
+    }
+});
+
+
+
+document.getElementById("hamburger_container_dir_arrow_tiger1").addEventListener("mousemove", function(e) {
+   
+   if(e.clientX > 100) {
+    document.getElementsByClassName("hamburger_container_dir_arrow1")[0].style.display="block";
+    document.getElementsByClassName("laptops_container_table")[0].style.display="block";
+           document.getElementsByClassName("laptops_container_table")[0].addEventListener("mouseleave", function() {
+            document.getElementsByClassName("hamburger_container_dir_arrow1")[0].style.display="none";
+            document.getElementsByClassName("laptops_container_table")[0].style.display="none";
+        })
+   } else {
+    document.getElementsByClassName("hamburger_container_dir_arrow1")[0].style.display="none";
+    document.getElementsByClassName("laptops_container_table")[0].style.display="none";
+   }
+   if(e.clientY > 215 || e.clientY < 195) {
+    document.getElementsByClassName("hamburger_container_dir_arrow1")[0].style.display="none";
+    document.getElementsByClassName("laptops_container_table")[0].style.display="none";
+    
+   }
+});
+
+document.getElementById("hamburger_container_dir_arrow_tiger2").addEventListener("mousemove", function(e) {
+  
+   if(e.clientX > 100) {
+    document.getElementsByClassName("hamburger_container_dir_arrow2")[0].style.display="block";
+    document.getElementsByClassName("electronic_container_table")[0].style.display="block";
+           document.getElementsByClassName("electronic_container_table")[0].addEventListener("mouseleave", function() {
+            document.getElementsByClassName("hamburger_container_dir_arrow2")[0].style.display="none";
+            document.getElementsByClassName("electronic_container_table")[0].style.display="none";
+        })
+   } else {
+    document.getElementsByClassName("hamburger_container_dir_arrow2")[0].style.display="none";
+    document.getElementsByClassName("electronic_container_table")[0].style.display="none";
+   }
+   if(e.clientY > 275 || e.clientY < 258) {
+    document.getElementsByClassName("hamburger_container_dir_arrow2")[0].style.display="none";
+    document.getElementsByClassName("electronic_container_table")[0].style.display="none";
+  
+   }
+});
+
+document.getElementById("hamburger_container_dir_arrow_tiger3").addEventListener("mousemove", function(e) {
+   
+   if(e.clientX > 100) {
+    document.getElementsByClassName("hamburger_container_dir_arrow3")[0].style.display="block";
+    document.getElementsByClassName("audio_container_table")[0].style.display="block";
+           document.getElementsByClassName("audio_container_table")[0].addEventListener("mouseleave", function() {
+            document.getElementsByClassName("hamburger_container_dir_arrow3")[0].style.display="none";
+            document.getElementsByClassName("audio_container_table")[0].style.display="none";
+        })
+   } else {
+    document.getElementsByClassName("hamburger_container_dir_arrow3")[0].style.display="none";
+    document.getElementsByClassName("audio_container_table")[0].style.display="none";
+   }
+   if(e.clientY > 340 || e.clientY < 320) {
+    document.getElementsByClassName("hamburger_container_dir_arrow3")[0].style.display="none";
+    document.getElementsByClassName("audio_container_table")[0].style.display="none";
+ 
+   }
+});
+
+document.getElementById("hamburger_container_dir_arrow_tiger4").addEventListener("mousemove", function(e) {
+  
+   if(e.clientX > 100) {
+    document.getElementsByClassName("hamburger_container_dir_arrow4")[0].style.display="block";
+    document.getElementsByClassName("smartphone_container_table1")[0].style.display="block";
+           document.getElementsByClassName("hamburger_sub_container")[0].addEventListener("mouseleave", function() {
+            document.getElementsByClassName("hamburger_container_dir_arrow4")[0].style.display="none";
+            document.getElementsByClassName("smartphone_container_table1")[0].style.display="none";
+        })
+   } else {
+    document.getElementsByClassName("hamburger_container_dir_arrow4")[0].style.display="none";
+    document.getElementsByClassName("smartphone_container_table1")[0].style.display="none";
+   }
+   if(e.clientY > 395 || e.clientY < 380) {
+    document.getElementsByClassName("hamburger_container_dir_arrow4")[0].style.display="none";
+    document.getElementsByClassName("smartphone_container_table1")[0].style.display="none";
+    
+   }
+});
+
+document.getElementById("hamburger_container_dir_arrow_tiger4").addEventListener("mousemove", function(e) {
+    console.log(e.clientY);
+   if(e.clientX > 100) {
+    document.getElementsByClassName("hamburger_container_dir_arrow4")[0].style.display="block";
+    document.getElementsByClassName("smartphone_container_table2")[0].style.display="block";
+           document.getElementsByClassName("hamburger_sub_container")[0].addEventListener("mouseleave", function() {
+            document.getElementsByClassName("hamburger_container_dir_arrow4")[0].style.display="none";
+            document.getElementsByClassName("smartphone_container_table2")[0].style.display="none";
+        })
+   } else {
+    document.getElementsByClassName("hamburger_container_dir_arrow4")[0].style.display="none";
+    document.getElementsByClassName("smartphone_container_table2")[0].style.display="none";
+   }
+   if(e.clientY > 395 || e.clientY < 380) {
+    document.getElementsByClassName("hamburger_container_dir_arrow4")[0].style.display="none";
+    document.getElementsByClassName("smartphone_container_table2")[0].style.display="none";
+    console.log("hii");
+   }
+});
+
+document.getElementsByClassName("mob_hamburger_btn")[0].addEventListener("click", function(){
+document.getElementsByClassName("mob_hamburger_btn")[0].style.transform = "rotate(45deg)";
+document.getElementsByClassName("nav_hamburger_content_division")[0].style.display="block";
+document.getElementsByClassName("nav_hamburger_division")[0].style.display="block";
+document.getElementsByClassName("nav_hamburger_content_division")[0].style.left="0px";
+});
+
+document.getElementsByClassName("nav_hamburger_division")[0].addEventListener("click", function() {
+document.getElementsByClassName("nav_hamburger_content_division")[0].style.left="-200px";
+document.getElementsByClassName("nav_hamburger_division")[0].style.display="none";
+document.getElementsByClassName("mob_hamburger_btn")[0].style.transition = "transform 0.5s";
+document.getElementsByClassName("mob_hamburger_btn")[0].style.transform = "rotate(0deg)";
+});
+
+document.getElementsByClassName("nav_hamburger_content_division")[0].addEventListener("click", function() {
+document.getElementsByClassName("nav_hamburger_content_division")[0].style.left="0px";
+});
+
+window.addEventListener("resize", function() {
+    if(window.innerWidth > 800) {
+        document.getElementsByClassName("nav_hamburger_content_division")[0].style.display="none";
+document.getElementsByClassName("nav_hamburger_division")[0].style.display="none";
+document.getElementsByClassName("mob_hamburger_btn")[0].style.transform = "rotate(0deg)";
+    }
+})
