239efa388eff1208a6583f90b3fb98bc32769c86
prakashz <mydreamzjobz@gmail.com>
2022-01-09 23:58:39 +0530

    filter container is created

---
+++ javascript/product.js
@@ -0,0 +1,39 @@
+document.getElementsByClassName("brands_container")[0].addEventListener("click", function(){
+if(document.getElementsByClassName("brand_list_container")[0].style.display == "none") {
+    document.getElementsByClassName("brand_list_container")[0].style.display = "block";
+    document.getElementById("arrow_of_product1").className = "fa fa-angle-up";
+} else {
+    document.getElementsByClassName("brand_list_container")[0].style.display = "none";
+    document.getElementById("arrow_of_product1").className = "fa fa-angle-down";
+}
+});
+
+document.getElementsByClassName("rating_container")[0].addEventListener("click", function(){
+    if(document.getElementsByClassName("rating_list_container")[0].style.display == "none") {
+        document.getElementsByClassName("rating_list_container")[0].style.display = "block";
+        document.getElementById("arrow_of_product2").className = "fa fa-angle-up";
+    } else {
+        document.getElementsByClassName("rating_list_container")[0].style.display = "none";
+        document.getElementById("arrow_of_product2").className = "fa fa-angle-down";
+    }
+    });
+
+    document.getElementsByClassName("colors_container")[0].addEventListener("click", function(){
+        if(document.getElementsByClassName("color_list_container")[0].style.display == "none") {
+            document.getElementsByClassName("color_list_container")[0].style.display = "block";
+            document.getElementById("arrow_of_product3").className = "fa fa-angle-up";
+        } else {
+            document.getElementsByClassName("color_list_container")[0].style.display = "none";
+            document.getElementById("arrow_of_product3").className = "fa fa-angle-down";
+        }
+        });
+
+        document.getElementsByClassName("price_container")[0].addEventListener("click", function(){
+            if(document.getElementsByClassName("price_bar_container")[0].style.display == "none") {
+                document.getElementsByClassName("price_bar_container")[0].style.display = "block";
+                document.getElementById("arrow_of_product4").className = "fa fa-angle-up";
+            } else {
+                document.getElementsByClassName("price_bar_container")[0].style.display = "none";
+                document.getElementById("arrow_of_product4").className = "fa fa-angle-down";
+            }
+            });
