98f4f08f11ade8d7729a29dab88f62ba9bd24f4f
prakashz <mydreamzjobz@gmail.com>
2022-02-14 00:54:18 +0530

    developed UI of notification_page

---
+++ notification_page.php
@@ -0,0 +1,75 @@
+<?php 
+include './action.php';
+$title = "Notification - Shopssy";
+include './header.php';
+
+?>
+
+
+    <!--sub navigation container start-->
+    <div class="sub_navigation_container">
+<center>
+    <div class="sub_navigation_inner_container">
+        <span><a href="./index.php">Home</a></span>
+        <span><i class="fas fa-arrow-right" style="color: #666666;font-size: 12px;"></i></span>
+        <span><a href="./notification_page.php">Notification</a></span>
+    </div>
+</center>
+    </div>
+    <!--sub navigation container end-->
+
+    <!--notification container start-->
+    <center>
+    <div class="notification_page_master_container">
+    <a href="#" class="notifi_link">
+        <div class="notification_page_parent_container">
+            <div class="notification_page_child_1">
+                <h1>S</h1>
+            </div>
+            <div class="notification_page_child_2">
+                <h2>Back In Stock!</h2>
+                <p>The yellow dress you were looking for is back in stock! Grab it NOW!</p>
+                <hr class="hr_line">
+                <h4>14/02/22 12:00AM</h4>
+            </div>
+        </div>
+    </a>
+
+    <a href="#" class="notifi_link">
+        <div class="notification_page_parent_container">
+            <div class="notification_page_child_1">
+                <h1>S</h1>
+            </div>
+            <div class="notification_page_child_2">
+                <h2>Back In Stock!</h2>
+                <p>The yellow dress you were looking for is back in stock! Grab it NOW!</p>
+                <hr class="hr_line">
+                <h4>14/02/22 12:00AM</h4>
+            </div>
+        </div>
+    </a>
+
+    <a href="#" class="notifi_link">
+        <div class="notification_page_parent_container">
+            <div class="notification_page_child_1">
+                <h1>S</h1>
+            </div>
+            <div class="notification_page_child_2">
+                <h2>Back In Stock!</h2>
+                <p>The yellow dress you were looking for is back in stock! Grab it NOW!</p>
+                <hr class="hr_line">
+                <h4>14/02/22 12:00AM</h4>
+            </div>
+        </div>
+    </a>
+    </div>
+    </center>
+    <!--notification container end-->
+
+    <?php 
+    include "./footer.php";
+    ?>
+              
+    <script src="./javascript/index.js"></script>
+</body>
+</html>
