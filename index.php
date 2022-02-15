346d95881b52cab076dd20848e5779053a09efe3
prakashz <mydreamzjobz@gmail.com>
2022-02-15 15:21:06 +0530

    email subscription functionality is developed

--- index.php
+++ index.php
@@ -1,6 +1,60 @@
 <?php 
 include './action.php';
 
+$user_mail_id = $_SESSION['user_login_email'];
+
+$check_email_sub_query = "SELECT * FROM `email_subscription` WHERE `user_email` = '$user_mail_id';";
+$check_email_sub_result = mysqli_query($con, $check_email_sub_query);
+$check_email_sub_row = mysqli_num_rows($check_email_sub_result);
+
+if($check_email_sub_row != 0) {
+
+$newsletter_retrieve_query = "SELECT * FROM `newsletter`;";
+$newsletter_retrieve_result = mysqli_query($con, $newsletter_retrieve_query);
+
+while($row = mysqli_fetch_assoc($newsletter_retrieve_result)) {
+$newsletter_s_id = $row['s_id'];
+$newsletter_html_data = $row['html_data'];
+$newsletter_title = $row['title'];
+$newsletter_subject = $row['subject'];
+}
+
+if(!isset($_COOKIE['N32SL33673R'])) {
+    $token_of_newsletter = "N32SL33673R";
+    setcookie($token_of_newsletter, 0, time() + (86400 * 730));    
+}
+
+if($_COOKIE['N32SL33673R'] != $newsletter_s_id) {
+    
+    require "./Mail/phpmailer/PHPMailerAutoload.php";
+    $mail = new PHPMailer;
+    $mail -> isSMTP();
+    $mail -> Host = 'smtp.gmail.com';
+    $mail -> Port = 587;
+    $mail -> SMTPAuth = true;
+    $mail -> SMTPSecure = 'tls';
+
+    $mail -> Username = 'shopssyz@gmail.com';
+    $mail -> Password = 'Shopssy$#@123';
+
+    $mail -> setFrom('shopssyz@gmail.com', "$newsletter_title");
+    $mail -> addAddress($_SESSION['user_login_email']);
+
+    $mail -> isHTML(true);
+    $mail -> Subject = "$newsletter_subject";
+    $mail -> Body = "$newsletter_html_data";
+
+    if(!$mail -> send()) {
+        echo "Sending Mail is Failed, Invalid Email";
+    } else {
+        $token_of_newsletter = "N32SL33673R";
+        setcookie($token_of_newsletter, $newsletter_s_id, time() + (86400 * 730), '/');  
+        
+    }
+}
+
+}
+
 $_SESSION['prod_qty'] = 1;
 unset($_SESSION['store_count']);
 
@@ -844,6 +898,8 @@ window.location.href = 'http://localhost:3000/product.php?sub_cat_identification
 
     <script src="./javascript/slider.js"></script>
     <script src="./javascript/index.js"></script>
+
+   
 </body>
 </html>
 
