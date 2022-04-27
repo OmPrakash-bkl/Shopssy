51a9d777840f257805830055768e3358927d88ca
prakashz <mydreamzjobz@gmail.com>
2022-04-27 19:03:42 +0530

    started sub category section

--- admin/index.php
+++ admin/index.php
@@ -180,7 +180,7 @@ mysqli_query($con, $delete_unfulfill_data_query);
 
   <div class="hamburger_link_section_inner_hidden_container hamburger_link_section_inner_hidden_container3">
   <div class="hamburger_link_section_inner_hidden_floating_container">
-  <button><i class="fa fa-eye"></i> View SubCat</button>
+  <button onclick="view_sub_cat()"><i class="fa fa-eye"></i> View SubCat</button>
   <span><i class="fa fa-arrow-circle-right"></i></span>
   </div>
   <div class="hamburger_link_section_inner_hidden_floating_container">
