a2ec5b0c90a25f48ed5c70876dc1440925db9acc
prakashz <mydreamzjobz@gmail.com>
2022-05-27 12:21:28 +0530

    create admin login form ui

---
+++ admin/admin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login | Shopssy</title>
+    <link rel="icon" href="../images/favicon1.png">
+    <script src="https://kit.fontawesome.com/628c629a17.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ceviche+One&family=Josefin+Sans:wght@700&family=Lobster&family=Pacifico&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="./css/admin_login.css">
+</head>
+<body>
+    <!-- Parent Container Start -->
+
+    <div class="parent_container">
+    
+    <!-- Login Box Start -->
+
+    <div class="login_box_container">
+
+    <center>
+    <h1 class="company_name">Shopssy</h1>
+    <form action="">
+        <input type="text" name="admin_username" class="input_form_boxes" placeholder="username"> <br>
+        <input type="password" name="admin_password" class="input_form_boxes" placeholder="password"> <br>
+        <button type="submit" id="admin_login_form_submission" name="admin_login_form_submission">LOGIN</button>
+    </form>
+    </center>
+
+    </div>
+
+    <!-- Login Box End -->
+    
+    </div>
+
+    <!-- Parent Container End -->
+</body>
+</html>
