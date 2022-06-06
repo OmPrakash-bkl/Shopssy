b95f156479b5fffe8313c797422769d7d38efbe4
prakashz <mydreamzjobz@gmail.com>
2022-06-06 21:12:21 +0530

    before blank page error

--- db_con.php
+++ db_con.php
@@ -1,25 +1,15 @@
 <?php 
-
 // Connect Functionality Start
-
 $serverName = 'localhost';
 $userName = 'root';
 $password = '';
 $db = 'shopssy_onlineshop';
-
-
 $con = mysqli_connect($serverName, $userName, $password, $db);
-
 // Connect Error Checker Fun Start
-
 if(!$con) {
     die("Connection Failed :" . mysqli_connect_error());
 }
-
-
-
 // Connect Error Checker Fun End
-
 // Connect Functionality End
 
 ?>
